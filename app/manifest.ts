f27068b1cd3f8868166d45fb5dfdc2f011c931a7
ArtemMikhailenko <Artemlend@gmail.com>
2025-12-26 02:49:47 +0200

    add fav icon and meta data and seo

--- app/manifest.ts
+++ app/manifest.ts
@@ -48,7 +48,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/icon-384.png',
@@ -59,7 +59,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
     shortcuts: [
